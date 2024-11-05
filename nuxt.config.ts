f75cdf1e41a741e6442643757521eb6a1565457d
fmoseley <fredmoseley@stanfordalumni.org>
2024-11-05 11:53:07 -0800

    Add environment variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,14 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui','@nuxtjs/tailwindcss'],
+  modules: ['@nuxt/ui', '@nuxtjs/tailwindcss'],
+  runtimeConfig: {
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    finchApiBaseUrl: process.env.FINCH_API_BASE_URL,
+  },
+  sourcemap: {
+    server: true,
+    client: true
+  }
 });
