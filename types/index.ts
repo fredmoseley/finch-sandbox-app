c4af0948d4405ab38d02857e8eeaa7a93b6d54bf
fmoseley <fredmoseley@stanfordalumni.org>
2024-11-06 12:17:04 -0800

    Add types for sandbox create api

---
+++ types/index.ts
@@ -0,0 +1,11 @@
+export interface SandboxCreateResponse {
+    access_token: string
+    payroll_provider_id: string,
+    company_id: string
+    sandbox_time: string
+  }
+  export interface SandboxCreateRequest {
+    provider_id: string,
+    products: string[],
+    emplyoee_size? : number
+  }
