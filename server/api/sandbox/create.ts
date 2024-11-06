3e830d6a20a9b296b3df3aa355cb187c750938c1
fmoseley <fredmoseley@stanfordalumni.org>
2024-11-06 14:38:31 -0800

    Add API call to create sandbox

---
+++ server/api/sandbox/create.ts
@@ -0,0 +1,41 @@
+import type { NuxtError } from 'nuxt/app'
+import Finch from '@tryfinch/finch-api'
+import type { SandboxCreateResponse } from '~/types'
+import { Product } from '~/utils/constants'
+
+export default defineEventHandler(async (event) => {
+  const { provider_id } = getQuery(event)
+  const { clientId, clientSecret, finchApiBaseUrl } = useRuntimeConfig()
+
+  const products = [Product.Company, Product.Directory, Product.Individual, Product.Employment]
+  const credentials = btoa(`${clientId}:${clientSecret}`) // Encode to Base64
+  const redis = useStorage('redis')
+
+  try {
+    const sandboxCreateResponse: SandboxCreateResponse = await $fetch(`${finchApiBaseUrl}/sandbox/create`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Basic ${credentials}`
+      },
+      body: {
+        provider_id,
+        products
+      }
+    })
+    const { access_token } = sandboxCreateResponse
+    await redis.setItem('access-token', access_token)
+    
+    return {
+      success: true
+    }
+  } catch (error) {
+    console.log('error', error)
+    const { statusCode, statusMessage, data } = error as NuxtError
+    throw createError({
+      statusCode: statusCode,
+      statusMessage: statusMessage,
+      data: data
+    })
+  }
+})
