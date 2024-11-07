d9643ff4c334d23006f92ea2d93c526398bb9e37
fmoseley <fredmoseley@stanfordalumni.org>
2024-11-07 11:03:30 -0800

    Improve testing mode

--- components/EmployerCompany.vue
+++ components/EmployerCompany.vue
@@ -1,92 +1,185 @@
 <script setup lang="ts">
 import type { Finch } from '@tryfinch/finch-api'
- import type { NuxtError } from 'nuxt/app'
- import { employerCompanyDataTest } from '~/utils/testdata'
+import type { NuxtError } from 'nuxt/app'
+import { employerCompanyDataTest, badCompanyDataTest } from '~/utils/testdata'
 
-const testing = ref<boolean>(false)
+const testing = ref<boolean>(true)
 const { provider } = defineProps<{ provider: string }>()
-const employerCompany = ref<any>({});
-onMounted(()=>{
-  console.log('EmployerCompany component mounted');
-  watchEffect(() => {
-  if (provider || testing.value) {
-    console.log('EmployerCompany watcheffect triggered', provider);
-    loadEmployerCompany();
+const employerCompany = ref<any>({})
+onMounted(() => {
+  console.log('EmployerCompany component mounted')
+  if (testing.value) {
+    employerCompany.value = employerCompanyDataTest
+  } else {
+    watchEffect(() => {
+      if (provider) {
+        console.log('EmployerCompany watcheffect triggered', provider)
+        loadEmployerCompany()
+      }
+    })
   }
-  })
 })
 
 async function loadEmployerCompany() {
-    try {
-        const employerCompanyResponse: any = await $fetch('/api/employer/company')
-        console.log('employerComanyResponse: ', employerCompanyResponse);
-        if (employerCompanyResponse) {
-            employerCompany.value = employerCompanyResponse
-        } else {
-            employerCompany.value = employerCompanyDataTest
-            testing.value
-             console.log('employerCompanyXXXX: ', employerCompany.value.id);
-        }
-    } catch(error) {
-        throw showError(error as NuxtError);
-    }
+  try {
+    const employerCompanyResponse: any = await $fetch('/api/employer/company')
+    console.log('employerComanyResponse: ', employerCompanyResponse)
+    employerCompany.value = employerCompanyResponse
+  } catch (error) {
+    throw showError(error as NuxtError)
+  }
 }
 </script>
 <template>
-    <div class="mb-8 text-3xl">
-      <h1>Employer Company</h1>
-      <h1 v-if="testing">TEST DATA</h1>
-    </div>
-  
-    <UForm v-if="employerCompany?.id" ref="form" :state="employerCompany" class="mb-16">
-      <UFormGroup label="Id" name="id" class="mb-4">
-        <UInput v-model="employerCompany.id"  readonly/>
-      </UFormGroup>
-  
-      <UFormGroup label="Legal Name" name="legal_name" class="mb-4">
-        <UInput v-model="employerCompany.legal_name as string" readonly />
-      </UFormGroup>
-  
-      <UFormGroup v-for="(value, key) in employerCompany.entity" :key="`entity-${key}`" label="Entity" name="entity" class="mb-4">
-          {{ key }}
-          <UInput v-model="employerCompany.entity[key]  as string" readonly/>
-        </UFormGroup>
-    
-      <UFormGroup label="Ein" name="ein" class="mb-4">
-        <UInput v-model="employerCompany.ein as string" readonly/>
-      </UFormGroup>
-  
-      <UFormGroup label="Primary Email" name="primary_email" class="mb-4">
-        <UInput v-model="employerCompany.primary_email as string" readonly/>
-      </UFormGroup>
-  
-      <UFormGroup v-if="employerCompany.primary_phone_number" label="Primary Phone Number" name="primary_phone_number" class="mb-4">
-        <UInput v-model="employerCompany.primary_phone_number as string" readonly/>
-      </UFormGroup>
-  
-      <UFormGroup v-for="(department, index) in employerCompany.departments" :key="index" label="departments" name="departments" class="mb-4">
-      <div v-if="department" v-for="(value, name ,index) in department" class="ml-4">
-        {{ name }}
-        <UInput v-if="typeof value === 'string'" v-model="department[name]  as string" readonly/>
-          <UInput v-else v-model="value.name  as string" readonly/>
-        </div>
+  <div class="mb-8 text-3xl">
+    <h1>Employer Company</h1>
+    <h1 v-if="testing">TEST DATA</h1>
+  </div>
+
+  <UForm
+    v-if="employerCompany?.id"
+    ref="form"
+    :state="employerCompany"
+    class="mb-16"
+  >
+    <UFormGroup
+      label="Id"
+      name="id"
+      class="mb-4"
+    >
+      <UInput
+        v-model="employerCompany.id"
+        readonly
+      />
     </UFormGroup>
 
-    <UFormGroup v-for="(location, outerIndex) in employerCompany.locations" :key="outerIndex" label="Locations" name="locations" class="mb-4">
-    <div v-if="location" v-for="(value, name ,index) in location" :key= "`${name}-${outerIndex}`" class="ml-4">
-         {{ name }}
-        <UInput v-model="location[name]  as string" readonly/>
+    <UFormGroup
+      label="Legal Name"
+      name="legal_name"
+      class="mb-4"
+    >
+      <UInput
+        v-model="employerCompany.legal_name as string"
+        readonly
+      />
+    </UFormGroup>
+
+    <UFormGroup
+      v-for="(value, key) in employerCompany.entity"
+      :key="`entity-${key}`"
+      label="Entity"
+      name="entity"
+      class="mb-4"
+    >
+      {{ key }}
+      <UInput
+        v-model="employerCompany.entity[key] as string"
+        readonly
+      />
+    </UFormGroup>
+
+    <UFormGroup
+      label="Ein"
+      name="ein"
+      class="mb-4"
+    >
+      <UInput
+        v-model="employerCompany.ein as string"
+        readonly
+      />
+    </UFormGroup>
+
+    <UFormGroup
+      label="Primary Email"
+      name="primary_email"
+      class="mb-4"
+    >
+      <UInput
+        v-model="employerCompany.primary_email as string"
+        readonly
+      />
+    </UFormGroup>
+
+    <UFormGroup
+      v-if="employerCompany.primary_phone_number"
+      label="Primary Phone Number"
+      name="primary_phone_number"
+      class="mb-4"
+    >
+      <UInput
+        v-model="employerCompany.primary_phone_number as string"
+        readonly
+      />
+    </UFormGroup>
+
+    <UFormGroup
+      v-for="(department, index) in employerCompany.departments"
+      :key="index"
+      label="departments"
+      name="departments"
+      class="mb-4"
+    >
+      <div
+        v-if="department"
+        v-for="(value, name, index) in department"
+        class="ml-4"
+      >
+        {{ name }}
+        <UInput
+          v-if="typeof value === 'string'"
+          v-model="department[name] as string"
+          readonly
+        />
+        <UInput
+          v-else
+          v-model="value.name as string"
+          readonly
+        />
       </div>
     </UFormGroup>
-    
-    <UFormGroup v-for="(account, outerIndex) in employerCompany.accounts" :key="outerIndex" label="Accounts" name="custom_fields" class="mb-4">
-      <div v-if="account" v-for="(value, name ,index) in account" :key="`${name}-${outerIndex}`" class="ml-4">
-          {{ name }}
-          <UInput v-model="account[name]  as string" readonly/>
-        </div>
+
+    <UFormGroup
+      v-for="(location, outerIndex) in employerCompany.locations"
+      :key="outerIndex"
+      label="Locations"
+      name="locations"
+      class="mb-4"
+    >
+      <div
+        v-if="location"
+        v-for="(value, name, index) in location"
+        :key="`${name}-${outerIndex}`"
+        class="ml-4"
+      >
+        {{ name }}
+        <UInput
+          v-model="location[name] as string"
+          readonly
+        />
+      </div>
     </UFormGroup>
-    </UForm>
-      
-      </template>
+
+    <UFormGroup
+      v-for="(account, outerIndex) in employerCompany.accounts"
+      :key="outerIndex"
+      label="Accounts"
+      name="custom_fields"
+      class="mb-4"
+    >
+      <div
+        v-if="account"
+        v-for="(value, name, index) in account"
+        :key="`${name}-${outerIndex}`"
+        class="ml-4"
+      >
+        {{ name }}
+        <UInput
+          v-model="account[name] as string"
+          readonly
+        />
+      </div>
+    </UFormGroup>
+  </UForm>
+</template>
 
 <style scoped></style>
--- components/EmployerDirectory.vue
+++ components/EmployerDirectory.vue
@@ -6,50 +6,64 @@ const { provider } = defineProps<{
   provider: string
 }>()
 
-const testing = ref<boolean>(false)
-const selected = ref<number[]>([]);
-const employerDirectory = ref<Finch.HRIS.Directory.IndividualInDirectory[]>([]);
+const testing = ref<boolean>(true)
+const selected = ref<number[]>([])
+const employerDirectory = ref<Finch.HRIS.Directory.IndividualInDirectory[]>([])
 // hide the checkbox column
 const tableUI = {
   checkbox: { padding: 'hidden' }
 }
 
-onMounted(()=>{
-  console.log('EmployerDirectory component mounted');
+onMounted(() => {
+  console.log('EmployerDirectory component mounted')
   watchEffect(() => {
-  if (provider) {
-    console.log('EmployerDirectory watcheffect triggered', provider);
-    loadEmployerDirectory();
-  }
+    if (provider) {
+      console.log('EmployerDirectory watcheffect triggered', provider)
+      loadEmployerDirectory()
+    }
   })
 })
 async function loadEmployerDirectory() {
   try {
-    const employerDirectoryResponse: Finch.HRIS.Directory.IndividualInDirectory[] = await $fetch('/api/employer/directory')
-    console.log('employerDirectoryResponse: ', employerDirectoryResponse);
+    const employerDirectoryResponse: Finch.HRIS.Directory.IndividualInDirectory[] =
+      await $fetch('/api/employer/directory')
+    console.log('employerDirectoryResponse: ', employerDirectoryResponse)
     employerDirectory.value = employerDirectoryResponse
     // employerDirectory.value = employeeDirectoryTest.individuals as Finch.HRIS.Directory.IndividualInDirectory[]
-  } catch(error) {
-    throw showError(error as NuxtError);
+  } catch (error) {
+    throw showError(error as NuxtError)
   }
 }
 
 function select(row: any) {
-  selected.value.length = 0;
-  selected.value.push(row);
-  console.log('selected: ', selected.value[0]);
-  navigateToEmployee(row.id);
+  selected.value.length = 0
+  selected.value.push(row)
+  console.log('selected: ', selected.value[0])
+  navigateToEmployee(row.id)
 }
 
 async function navigateToEmployee(employeeId: string) {
-
-  await navigateTo({
-      path: `/employee/${employeeId}`,
+  try {
+    await navigateTo({
+      path: `/employee/${employeeId}`
     })
+  } catch (error) {
+    throw showError(error as NuxtError)
+  }
 }
 </script>
 
 <template>
-  <h1 class="mb-8 text-3xl" v-if="testing">TEST DATA</h1>
-  <UTable v-model="selected" :ui="tableUI" :rows="employerDirectory" @select="select" />
+  <h1
+    class="mb-8 text-3xl"
+    v-if="testing"
+  >
+    TEST DATA
+  </h1>
+  <UTable
+    v-model="selected"
+    :ui="tableUI"
+    :rows="testing ? employeeDirectoryTest.individuals : employerDirectory"
+    @select="select"
+  />
 </template>
