bf27fa5008c854dced1daf5dd424bb4e1c304455
fmoseley <fredmoseley@stanfordalumni.org>
2024-11-06 22:17:46 -0800

    Create the employee details page

---
+++ pages/employee/[id].vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const employeeId = useRoute().params.id as string
+console.log('employeeId: ', employeeId)
+</script>
+<template>
+  <div>
+    <EmployeePersonalData :employeeId="employeeId" />
+    <EmployeeEmployment :employeeId="employeeId"/>
+  </div>
+</template>
+
+<style scoped></style>
