4af28ee12ecee1b5be1bf89dceeb6cf7eb762a44
fmoseley <fredmoseley@stanfordalumni.org>
2024-11-12 13:07:35 -0800

    Update the typescript types

--- server/api/sandbox/create.ts
+++ server/api/sandbox/create.ts
@@ -5,31 +5,36 @@ import { Product } from '~/utils/constants'
 
 export default defineEventHandler(async (event) => {
   const { provider_id } = getQuery(event)
-  const { clientId, clientSecret, finchApiBaseUrl } = useRuntimeConfig()
-
+  const { clientId, clientSecret, finchApiBaseUrl, finchApiVersion } = useRuntimeConfig()
   const products = [Product.Company, Product.Directory, Product.Individual, Product.Employment]
+
+  const requestBody: Finch.Sandbox.ConnectionCreateParams = {
+    provider_id: provider_id as string,
+    products
+  }
   const credentials = btoa(`${clientId}:${clientSecret}`) // Encode to Base64
   const redis = useStorage('redis')
 
   try {
-    const sandboxCreateResponse: SandboxCreateResponse = await $fetch(`${finchApiBaseUrl}/sandbox/connections`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Basic ${credentials}`
-      },
-      body: {
-        provider_id,
-        products
+    const sandboxCreateResponse: Finch.Sandbox.ConnectionCreateResponse = await $fetch(
+      `${finchApiBaseUrl}/sandbox/connections`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Basic ${credentials}`,
+          'Finch-API-Version': finchApiVersion
+        },
+        body: requestBody
       }
-    })
+    )
     const { access_token } = sandboxCreateResponse
     await redis.setItem('access-token', access_token)
-    
+
     return {
       success: true
     }
-  } catch (error) {
+  } catch (error: unknown) {
     const { statusCode, statusMessage, data } = error as NuxtError
     throw createError({
       statusCode: statusCode,
